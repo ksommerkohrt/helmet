f7d1202ae42862eab0121558e11761c02e6334ad
James Goppert <james.goppert@gmail.com>
2023-09-14 01:36:14 -0400

    Update native install.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- install/native_install.sh
+++ install/native_install.sh
@@ -2,13 +2,18 @@
 set -e
 set -x
 
+pushd ~/cognipilot/helmet/install
+
 ./scripts/base.sh
 ./scripts/zephyr.sh
 ./scripts/ros.sh
 ./scripts/gazebo.sh
 ./scripts/user_setup.sh native
 
+
 # install scripts
 cp ./resources/build_mrbuggy3_sitl ~/bin
 cp ./resources/cyecca ~/bin
 cp ./resources/docs ~/bin
+
+popd
