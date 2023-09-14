999468ecefc4f912df9ec040d4f7a3b0da15c71f
James Goppert <james.goppert@gmail.com>
2023-09-14 02:11:32 -0400

    Wrap YCM build.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- install/scripts/base.sh
+++ install/scripts/base.sh
@@ -79,6 +79,9 @@ echo $whoami
 sudo chown -R $UID:$UID /opt/vim
 if ! [ -d /opt/vim/YouCompleteMe ]; then
   git clone --recurse-submodules https://github.com/ycm-core/YouCompleteMe.git /opt/vim/YouCompleteMe
+  pushd /opt/vim/YouCompleteMe
+  ./install.py --clangd-completer
+  popd
 fi
 if ! [ -d /opt/vim/NERDCommenter ]; then
   git clone https://github.com/preservim/nerdcommenter.git /opt/vim/NERDCommenter
@@ -86,8 +89,4 @@ fi
 if ! [ -d /opt/vim/securemodelines ]; then
   git clone https://github.com/ciaranm/securemodelines /opt/vim/securemodelines
 fi
-pushd /opt/vim/YouCompleteMe
-./install.py --clangd-completer
-popd
-
 # vi: ts=2 sw=2 et
