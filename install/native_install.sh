317645e7035db767512c0189d9411ae8151a6781
James Goppert <james.goppert@gmail.com>
2023-09-14 03:19:35 -0400

    Update install order.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- install/native_install.sh
+++ install/native_install.sh
@@ -5,10 +5,6 @@ set -x
 pushd ~/cognipilot/helmet/install
 
 ./scripts/base.sh
-./scripts/zephyr.sh
-./scripts/ros.sh
-./scripts/gazebo.sh
-./scripts/user_setup.sh native
 
 # install zeth config
 sudo mkdir -p /opt/zeth
@@ -22,6 +18,12 @@ if ! [ -f  /opt/zeth/net-setup.sh ]
 then
   sudo cp ./resources/net-setup.sh /opt/zeth
 fi
+
+./scripts/zephyr.sh
+./scripts/ros.sh
+./scripts/gazebo.sh
+./scripts/user_setup.sh native
+
 
 # install scripts
 cp ./resources/build_mrbuggy3_sitl ~/bin
