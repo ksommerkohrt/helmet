9518b50d865f4a5b674f12ec6ae7f4e77af4986b
Benjamin Perseghetti <bperseghetti@rudislabs.com>
2023-10-18 00:23:31 -0400

    Use GAZEBO_VERSION throughout install process.
    Prep for Harmonic sync.
    
    Signed-off-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>

--- install/scripts/gazebo.sh
+++ install/scripts/gazebo.sh
@@ -12,15 +12,11 @@ if ! [ -f /etc/apt/sources.list.d/gazebo-stable.list ]; then
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
 fi
 
-if ! [ -f /etc/apt/sources.list.d/gazebo-prerelease.list ]; then
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-prerelease $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-prerelease.list > /dev/null
-fi
-
 sudo apt-get -y update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
   gz-${GAZEBO_VERSION} \
-  ros-humble-ros-gzgarden-bridge \
-  ros-humble-ros-gzgarden-image \
-  ros-humble-ros-gzgarden-sim
+  ros-humble-ros-gz${GAZEBO_VERSION}-bridge \
+  ros-humble-ros-gz${GAZEBO_VERSION}-image \
+  ros-humble-ros-gz${GAZEBO_VERSION}-sim
 
 # vi: ts=2 sw=2 et
